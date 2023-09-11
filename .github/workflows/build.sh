c6ee8b5fb2560266cfa057ceb476712d2488e6aa
Matthias Hochsteger <mhochsteger@cerbsim.com>
2023-09-11 16:29:15 +0200

    host full pyodide repo

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -1,4 +1,4 @@
-wget https://github.com/pyodide/pyodide/releases/download/0.24.0a1/pyodide-core-0.24.0a1.tar.bz2
+wget https://github.com/pyodide/pyodide/releases/download/0.24.0a1/pyodide-0.24.0a1.tar.bz2
 tar xvf pyodide-core-0.24.0a1.tar.bz2
 mv pyodide/pyodide-lock.json pyodide/pyodide-lock.json.bak
 wget https://cdn.jsdelivr.net/pyodide/dev/full/pyodide-lock.json -O pyodide/pyodide-lock.json
