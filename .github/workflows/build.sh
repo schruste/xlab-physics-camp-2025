6ea62fb161739d28c2d8b909bc0e90ee5d93893e
Matthias Hochsteger <mhochsteger@cerbsim.com>
2023-09-11 17:13:21 +0200

    remove some large, unused packages

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -1,5 +1,6 @@
 wget https://github.com/pyodide/pyodide/releases/download/0.24.0a1/pyodide-0.24.0a1.tar.bz2
 tar xvf pyodide-0.24.0a1.tar.bz2
+cd pyodide && rm -f astropy-* Fiona-* bokeh-* gensim-* mypy-* opencv_python-* pandas-* scikit-* scikit_image-* scikit_learn-* sympy-* test-* yt-* && cd ..
 #mv pyodide/pyodide-lock.json pyodide/pyodide-lock.json.bak
 #wget https://cdn.jsdelivr.net/pyodide/dev/full/pyodide-lock.json -O pyodide/pyodide-lock.json
 python .github/workflows/merge.py https://ngsolve.org/ngslite/static/pyodide/pyodide-lock.json pyodide/pyodide-lock.json
